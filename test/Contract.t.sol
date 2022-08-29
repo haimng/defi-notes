20ad4b5e867ecf956fb6653f618f42b687916f2d
t4sk <tsk.nakamura@gmail.com>
2022-08-30 05:50:27 +0900

    counter test

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -2,11 +2,32 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+contract Counter {
+    uint public count;
+
+    function inc() external {
+        count++;
+    }
+}
 
 contract ContractTest is Test {
-    function setUp() public {}
+    Counter private counter = new Counter();
 
-    function testExample() public {
+    function setUp() public {
+        console.log("setup - count", counter.count());
+    }
+
+    function testExample1() public {
+        counter.inc();
+        console.log("test 1 - count", counter.count());
+        assertTrue(true);
+    }
+
+    function testExample2() public {
+        counter.inc();
+        console.log("test 2 - count", counter.count());
         assertTrue(true);
     }
 }
