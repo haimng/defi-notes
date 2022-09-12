d891ebfcb83c0cad67433730801cc9d604b8d689
t4sk <tsk.nakamura@gmail.com>
2022-09-12 10:45:03 +0900

    test

--- test/UniswapV3Flash.test.sol
+++ test/UniswapV3Flash.test.sol
@@ -7,7 +7,7 @@ import "forge-std/console.sol";
 import "../src/UniswapV3Flash.sol";
 
 
-contract UniV3Test is Test {
+contract UniV3FlashTest is Test {
     address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
     uint24 constant POOL_FEE = 3000;
